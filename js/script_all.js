50d70d17d015dbdaa4a3ebe323c98fb36e0756e7
Joab Mendes <joab.mendes.r2d2@gmail.com>
2013-09-05 02:22:08 -0300

    update commit

---
+++ js/script_all.js
@@ -0,0 +1,8 @@
+
+
+function checkInsertPatern(){
+	blackberry.ui.dialog.standardAskAsync("In your first login, you need insert a pattern,\n click help to see how insert a pattern", blackberry.ui.dialog.D_OK, null, {
+			title: "Welcome to the Trunkeys!"
+		});
+}
+
